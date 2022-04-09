d937bfeaa717f2a41648a4ac356bd9a53eaf3c4f
root <thedevopslearning@gmail.com>
2022-04-09 02:20:07 +0000

    First trial

---
+++ Readme.md
@@ -0,0 +1,5 @@
+This is a read me file
+
+The script in the repo is to create users and do some other task
+You can run the using "bash scrip.sh"  or "./script.sh"
+Thank you
---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Welcome to scripting lifestyle"
+echo
+echo
+sleep 5
+echo "If you like i can creat a user with a folder for you"
+sleep 2 
+echo
+echo
+echo "Would you like me to go ahead and create a personal user for you"
